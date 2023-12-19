2351efe41c866825f385483d26b8873aeba9bcef
Benedict Schwier <32scbe1dpt@hft-stuttgart.de>
2023-12-19 17:40:53 +0100

    004 - basic impl of frontend; added dockerfile for frontend;

--- src/main/java/com/example/bennidemo/BennidemoApplication.java
+++ src/main/java/com/example/bennidemo/BennidemoApplication.java
@@ -2,9 +2,22 @@ package com.example.bennidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BennidemoApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BennidemoApplication.class, args);
